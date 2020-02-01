8b2eb8661f61e9e70071b364cad1b4b782879327
Chris <christopher0chung@gmail.com>
2020-02-01 18:38:30 -0500

    Explosion scene added with example.

---
+++ Assets/Resources/ExplosionResources/ExplosionController.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ExplosionController : MonoBehaviour
+{
+    GameObject explosionatorPrefab;
+
+    private void Awake()
+    {
+        explosionatorPrefab = Resources.Load<GameObject>("ExplosionResources/Explosionator");
+    }
+
+    public void MakeExplosion(Vector3 where, ExplosionSize size, List<Texture> limbs)
+    {
+        GameObject g = Instantiate<GameObject>(explosionatorPrefab, transform);
+        g.transform.position = where;
+        g.GetComponent<Explosionator>().FireEffect(where, size, limbs);
+    }
+}
