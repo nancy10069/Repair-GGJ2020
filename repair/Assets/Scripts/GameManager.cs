44af636c4d3be7ac5e6f02caa3b6c3aa356a0256
nancy <nancy10069@gmail.com>
2020-02-01 01:47:52 -0500

    Add Tutorial Level

---
+++ repair/Assets/Scripts/GameManager.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    public int level=0;//current level
+    public static GameManager instance;
+    // Start is called before the first frame update
+    void Awake(){
+        instance=this;
+    }
+    void Start()
+    {
+        
+    }
+    public void nextLevel(){level++;
+    renderLevel();
+    }
+    void renderLevel(){
+        //reset
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
