d6b1d7ab0b8a05d8402ed9ca406f9a1ece759b20
nancy <nancy10069@gmail.com>
2020-02-01 18:17:28 -0500

    Adding Dialogue and Menu (Start Scene is Scene0)

---
+++ Assets/DialogueManager.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DialogueManager : MonoBehaviour
+{
+
+    //level 1,2,3,4
+    public List<string[]>diags=new List<string[]>{
+        new string[]{"111111","22222"},
+        new string[]{"133311111","22222"},
+        new string[]{"11114411","22222"},
+        new string[]{"111155511","22222"},
+
+    };
+    // Start is called before the first frame update
+    void Start()
+    {
+        renderText();
+    }
+    void renderText(){
+        text.text = diags[GameManager.instance.level][currNode];
+    }
+    public LevelManager level;
+    public void next(){
+        currNode++;
+        
+        if (currNode==diags[GameManager.instance.level].Length){
+            gameObject.SetActive(false);
+        level.startGame();
+        }else{
+                    renderText();
+
+        }
+    }
+    public UnityEngine.UI.Text text;
+
+    int currNode=0;
+
+    
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyUp(KeyCode.Space)){next();}
+    }
+}
