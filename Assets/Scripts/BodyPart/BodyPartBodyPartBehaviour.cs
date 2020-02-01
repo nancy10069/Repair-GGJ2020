3a0dfe9faa981307030d39385eb6a119252721ea
Zed <yanjingzhaisun@gmail.com>
2020-02-01 11:27:39 -0500

    applied receive type data

--- Assets/Scripts/BodyPart/BodyPartBodyPartBehaviour.cs
+++ Assets/Scripts/BodyPart/BodyPartBodyPartBehaviour.cs
@@ -23,7 +23,7 @@ public class BodyPartBehaviour : MonoBehaviour
     }
 
     // Update is called once per frame
-    void Update()
+    protected virtual void Update()
     {
         AllObjectInSceneInteraction();
     }
--- Assets/Scripts/ObjectReaction/ObjectReaction.cs
+++ Assets/Scripts/ObjectReaction/ObjectReaction.cs
@@ -4,5 +4,6 @@ using UnityEngine;
 
 public abstract class ObjectReaction : MonoBehaviour
 {
+    public InteractionAction.InteractionActionType receiveType;
     public abstract void TriggerReaction(BodyPartBehaviour from, InteractionAction.InteractionActionType actionType, string jsonParam);
 }
