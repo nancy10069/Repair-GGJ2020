6b643b4b97348c49150ac29db3d61e26ea8d6c37
Zed <yanjingzhaisun@gmail.com>
2020-02-01 11:57:14 -0500

    fix error

--- Assets/Scripts/ObjectReaction/ObjectReaction.cs
+++ Assets/Scripts/ObjectReaction/ObjectReaction.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 
 [RequireComponent(typeof(Rigidbody2D))]
 [RequireComponent(typeof(Collider2D))]
-[RequireComponent(typeof(PhysicsInteractionInfo)]
+[RequireComponent(typeof(PhysicsInteractionInfo))]
 public abstract class ObjectReaction : MonoBehaviour
 {
     protected Rigidbody2D rigidBody;
