e33bc6fca9e48f835faff476ba55f793a3201a3b
Zed <yanjingzhaisun@gmail.com>
2020-02-01 11:49:33 -0500

    added destroy reaction

---
+++ Assets/Scripts/ObjectReaction/DestroyReaction.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DestroyReaction : ObjectReaction
+{
+
+    [SerializeField]
+    public GameObject destroyParticleEffect;
+    public override void TriggerReaction(BodyPartBehaviour from, InteractionAction.InteractionActionType actionType, string jsonParam)
+    {
+        if (actionType == InteractionAction.InteractionActionType.Destroy)
+        {
+            Instantiate(destroyParticleEffect, transform.position, Quaternion.identity);
+            Destroy(gameObject);
+
+        }
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
