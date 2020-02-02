db2719e35279e824253cf23baebbb3f7b2295fac
Zijian Zhou <yanjingzhaisun@gmail.com>
2020-02-02 13:24:38 -0500

    merge

--- Assets/DestinationController.cs
+++ Assets/DestinationController.cs
@@ -1,22 +1,24 @@
-﻿using System.Collections;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using UnityEngine;
 
 public class DestinationController : MonoBehaviour
-{LevelManager level;
+{
+    LevelManager level;
     // Start is called before the first frame update
     void Start()
     {
-     level=GameObject.Find("LevelManager").GetComponent<LevelManager>();   
+        level = GameObject.Find("LevelManager").GetComponent<LevelManager>();
     }
-    private void OnCollisionEnter2D(Collision2D collider){
-        if (collider.gameObject.name=="Chara"){
-           level.end();
+    private void OnTriggerEnter2D(Collider2D collider)
+    {
+        if (collider.gameObject.name == "Chara")
+        {
+            level.end();
         }
     }
     // Update is called once per frame
     void Update()
     {
-        
+
     }
 }
